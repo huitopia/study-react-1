283296ea597284028e6aec144ac214ba00249205
huitopia <huitopia27@gmail.com>
2024-05-09 15:07:54 +0900

    js object

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 function App() {
-  return <></>;
+  // object : {}
+  //   {} 안에 property 명 : property 값 쌍들이 작성됨
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "sinchon", city: "seoul", country: "korea" };
+  a = { b, c };
+  console.log(a);
+  console.log(b);
+  return <div></div>;
 }
 export default App;
