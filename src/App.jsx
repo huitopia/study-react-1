dd1680aec013035dc102090a1ce8bfed79dc4b27
huitopia <huitopia27@gmail.com>
2024-05-09 15:11:01 +0900

    js object 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 function App() {
-  // object : {}
-  //   {} 안에 property 명 : property 값 쌍들이 작성됨
-  let a = {};
-  let b = { name: "흥민" };
-  let c = { address: "sinchon", city: "seoul", country: "korea" };
-  a = { b, c };
-  console.log(a);
-  console.log(b);
-  return <div></div>;
+  const a = { name: "흥민", team: "토트넘", salary: 500 };
+  return (
+    <div>
+      {a.name}, {a.team}, {a.salary}
+    </div>
+  );
 }
+
 export default App;
