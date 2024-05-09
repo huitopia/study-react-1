a65a0028036ca61f06a34b7022afc52f5ab45d4b
huitopia <huitopia27@gmail.com>
2024-05-09 12:07:55 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
 function App() {
-  const myTag = <h1>hello</h1>;
-  const val1 = "채채";
-
-  // {} : jsx 내에서 javascript 코드 사용 법
-  // boolean, null, undefined 출력 불가능
-  // array [2, 3] 은 값(23)만 출력
-  // object 사용 시 property 명 작성하여 값만 출력
-  return (
-    <>
-      <div>{myTag}</div>
-      <div>{val1}</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
