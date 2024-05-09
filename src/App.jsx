60dd40726d18d6cf179162ee959d5b430a3b3b38
huitopia <huitopia27@gmail.com>
2024-05-09 14:30:10 +0900

    falsy, truthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App() {
+  /*
+  falsy(false 같은)
+    false, 0, "", null, undefined, Nan
+  */
+  const a = 0;
+  if (a) {
+    console.log("a는 참이다");
+  } else {
+    console.log("a는 거짓이다");
+  }
+  /*
+  truthy(true 같은)
+    falsy 가 아니면 truthy
+  */
+
+  let b = 0;
+  // 앞이 false 니까 false 로 결정
+  let c = b && "값이 있음";
+  console.log(c);
+
+  let d = b || "값이 없음";
+  console.log(d);
+
   return <></>;
 }
-
 export default App;
