c555c780376c324fe0c259b2576df2a1843d05e3
huitopia <huitopia27@gmail.com>
2024-05-09 16:24:39 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import React from "react";
-
-function App(props) {
-  const a = ["L", "K", "J"];
-  const b = a.map(function (item) {
-    return item + "team";
-  });
-  const c = a.map(function (item) {
-    return <li>{item}</li>;
-  });
-  return (
-    <div>
-      {b[0]},{c}
-    </div>
-  );
+function App() {
+  return <div></div>;
 }
 
 export default App;
