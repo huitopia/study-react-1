b3ea4700c694492fdc46796e1c8a40ccb14f2ec4
huitopia <huitopia27@gmail.com>
2024-05-09 11:38:48 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,5 @@
-// 최상위 컴포넌트는 1개만 작성
-function MyComponent() {
-  // 최상위 컴포넌트 사용하지 않을 때 fragment 사용
-  return (
-    <>
-      <p>Lorem ipsum dolor.</p>
-      <p>Consequuntur, dolorem, perspiciatis!</p>
-    </>
-  );
-}
-function MyComp() {
-  // 여러줄 사용 시 div 로 감싸서 사용
-  return (
-    <div>
-      <p>1</p>
-      <p>2</p>
-    </div>
-  );
-}
-
-function MyTag() {
-  // JSX 코드가 여러 줄일 때 () 사용
-  return (
-    <div>
-      hello Tag
-      <p>Lorem ipsum.</p>
-      <p>Expedita, necessitatibus!</p>
-    </div>
-  );
-}
 function App() {
-  return (
-    <div>
-      <MyTag />
-      <MyComp />
-      <MyComponent />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
