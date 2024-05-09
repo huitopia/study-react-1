123ee0868d08cc3dc1dc9f0954d36170e8fe9280
huitopia <huitopia27@gmail.com>
2024-05-09 12:28:24 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
 function App() {
-  // {} 사용해서 간단한 js code 작성 가능
-  const v1 = 5;
-  const v2 = 10;
-  function someAction() {
-    return "chae";
-  }
-  function someAction2(p) {
-    return p * p;
-  }
-  return (
-    <>
-      <p>{someAction()}</p>
-      <p>{someAction2(5)}</p>
-      <p>{v1 < 10 ? "small" : "big"}</p>
-      <p>{v1 < 3 ? "small" : "big"}</p>
-      <p>
-        {v1} + {v2} = {v1 + v2}
-      </p>
-      <p>
-        {v1} * {v2} = {v1 * v2}
-      </p>
-      <p>
-        {v1} / {v2} = {v1 / v2}
-      </p>
-      <p>
-        {v1} % {v2} = {v1 % v2}
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
