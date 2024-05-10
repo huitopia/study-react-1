9decd870282584a51e278652a1752c77a802ce75
huitopia <huitopia27@gmail.com>
2024-05-10 12:12:16 +0900

    깊은 복사를 얕은 복사 여러번으로 해결

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,20 @@ function App(props) {
   b.age = 44;
   console.log("b.age> ", b.age); // 44
   console.log("a.age> ", a.age); // 33
+
   // address : 객체지만 참조 값을 가지고 있음
   // b로 복사를 해도 b와 a는 같은 address를 갖고 있음
   b.address.city = "busan";
   console.log("a.address.city > ", a.address.city); // busan
   console.log("b.address.city > ", b.address.city); // busan
-  // 깊은 복사(deep copy)
+
+  // 깊은 복사(deep copy) : 얕은 복사 여러번
+  const { ...c } = a;
+  const { ...address1 } = a.address;
+  c.address = address1;
+  c.address.city = "seoul";
+  console.log("c.address.city > ", c.address.city); // seoul
+  console.log("a.address.city > ", a.address.city); // busan
   return <div></div>;
 }
 
