99d183c677ad03e147b39a69500cde2a7e1ab7ce
huitopia <huitopia27@gmail.com>
2024-05-10 15:44:23 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <button
-        onClick={() => console.log("clicked")}
-        onMouseEnter={() => console.log("mouse entered")}
-        onMouseLeave={() => console.log("mouse leave")}
-      >
-        click1
-      </button>
-      <input type="text" onChange={() => console.log("changed")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
