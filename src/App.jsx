ce6dc538ea1f1c3a55c99bf6c5af4e1bfca026f9
huitopia <huitopia27@gmail.com>
2024-05-10 16:08:38 +0900

    style props 객체로, css property 명은 lowerCamelCase로 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // style props
+  return (
+    <div>
+      <div
+        style={{
+          border: "5px solid blue",
+          backgroundColor: "green",
+          color: "white",
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
 }
 
 export default App;
