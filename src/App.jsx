0246ef0929c3ae334c08ac179d3eabe698d15e03
huitopia <huitopia27@gmail.com>
2024-05-10 16:09:18 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // style props
-  return (
-    <div>
-      <div
-        style={{
-          border: "5px solid blue",
-          backgroundColor: "green",
-          color: "white",
-          fontSize: "2rem",
-          textAlign: "center",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
