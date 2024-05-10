35d48142d8265051c670dabd84f295c9b07f62fa
huitopia <huitopia27@gmail.com>
2024-05-10 14:22:21 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import React from 'react';
-function MyComp(props) {
-  return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.age}</p>
-      <p>{props.address}</p>
-      <p>{props.city}</p>
-      <p>{props.country}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp name="k" age={33} address="seoul"></MyComp>
-      <MyComp city="busan" country="USA"></MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
