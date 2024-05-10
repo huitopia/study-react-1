e999fe900f9dfb1463aaf0c26a1be232716c0f60
huitopia <huitopia27@gmail.com>
2024-05-10 15:24:31 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  console.log("name> ", props.name);
-  console.log("somProp> ", props.somProp); // props의 빈 값 true
-  console.log("otherProp> ", props.otherProp); // props가 없으면 undefined
+function App(props) {
   return <div></div>;
 }
 
-function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={30} somProp />
-    </div>
-  );
-}
 export default App;
