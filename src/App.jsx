e412f8c1552bffe1e1c193a79360603a6eb4fd70
huitopia <huitopia27@gmail.com>
2024-05-10 16:17:36 +0900

    style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+function MyComp({ headColor, bodyColor }) {
+  return (
+    <div>
+      <h1
+        style={{
+          color: headColor,
+        }}
+      >
+        Lorem.
+      </h1>
+      <p
+        style={{
+          color: bodyColor,
+        }}
+      >
+        Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  );
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp headColor={"green"} bodyColor={"blue"} />
+    </div>
+  );
 }
 
 export default App;
