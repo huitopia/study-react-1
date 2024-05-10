a3176da6ef05c1c2f79aaf5359ce45f5834bf3e5
huitopia <huitopia27@gmail.com>
2024-05-10 14:30:04 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyElem(props) {
-  console.log(props);
-  const { name, age } = props;
-  console.log("name > ", name);
-  console.log("age > ", age);
-  return <div>{props.name}</div>;
-}
 function App(props) {
-  return (
-    <div>
-      <MyElem name="k" age={20} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
