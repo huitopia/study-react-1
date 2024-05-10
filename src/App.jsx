eafe75edf0c59124e8a385580799c57415ae6c8c
huitopia <huitopia27@gmail.com>
2024-05-10 14:43:19 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, team = "무소속" }) {
-  return (
-    <div>
-      <ul>
-        <li>이름 : {name}</li>
-        <li>age : {age}</li>
-        <li>team : {team}</li>
-      </ul>
-    </div>
-  );
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp name="k" age={30} team="jj" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
