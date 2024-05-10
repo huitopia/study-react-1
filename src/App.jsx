3ef9d1507ce6650a2994f55fa580ffaaa9705566
huitopia <huitopia27@gmail.com>
2024-05-10 14:29:36 +0900

    props를 분해할당으로 받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+function MyElem(props) {
+  console.log(props);
+  const { name, age } = props;
+  console.log("name > ", name);
+  console.log("age > ", age);
+  return <div>{props.name}</div>;
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyElem name="k" age={20} />
+    </div>
+  );
 }
 
 export default App;
