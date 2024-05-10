06e8c892e3f3f2aa1d27e7f3fb7d9bdba0ae109e
huitopia <huitopia27@gmail.com>
2024-05-10 10:42:13 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // destructuring assignment
-  // (구조) 분해 할당
-
-  // object({}, [])를 분해해서 할당
-  const a = {
-    name: "chae",
-    age: 27,
-  };
-  // 기존 할당
-  const aName = a.name;
-  // 구조 분해 할당
-  const { name: bName, age: bAge } = a;
-  console.log("bName = ", bName);
-  console.log("age = ", bAge);
-  // 오른쪽항 객체의 프로퍼티명과 할당받는 변수명이 같으면 변수명 생략 가능
-  const { name: name, age: age } = a;
-  // const {name, age} = a
-  console.log("name = ", name);
-  console.log("age = ", age);
   return <div></div>;
 }
 
