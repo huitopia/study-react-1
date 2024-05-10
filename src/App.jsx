cbcaddcf7d4816b4696b0d269ed2ade6d5ee9d2e
huitopia <huitopia27@gmail.com>
2024-05-10 09:23:57 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-function App() {
-  // String
-  const s1 = "some string";
-  const s2 = "other string";
-  const s3 = "I'm 신뢰";
-  const s4 = '제 이름은 "흥민"입니다.';
-  const s5 = `another string`; // template literal
-  const s6 = `s1은 ${s1}, s2는 ${s2}`;
-  console.log(s6);
-  return <div>{}</div>;
+import React from "react";
+
+function App(props) {
+  return <div></div>;
 }
 
 export default App;
