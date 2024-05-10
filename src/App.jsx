dabd82d5d090ce98ff589197e3a8c653691c8fa3
huitopia <huitopia27@gmail.com>
2024-05-10 14:15:17 +0900

    props(properties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import React from 'react';
+function MyButton() {
+  return (
+    <button
+      style={{
+        padding: '10px',
+        backgroundColor: 'green',
+        color: 'white',
+      }}
+    >
+      {props.name}
+    </button>
+  );
+}
 
+// props : properties
+// <MyButton name="click3" />
+// props: {name: "click3"}
 function App(props) {
-  // object({}, [])
-  const a = [6, 7, 8];
-  const [x, y, z] = a;
-  console.log("x>", x);
-  console.log("y>", y);
-  console.log("z>", z);
-
-  // 나머지 모두 (...)
-  const [s, ...t] = a;
-  console.log("s>", s); // 6
-  console.log("t>", t); // [7, 8]
-  console.log("t[0]>", t[0]); // 7
-  console.log("t[1]>", t[1]); // 8
-
-  // 나머지 모두로 배열 복사 가능
-  const [...b] = a; // 각 원소를 새 배열에 복사 (참조값 복사 X)
-  console.log("b> ", b); // [6,7,8]
-
-  // 연습 : 객체 복사
-  const d = ["pizza", "son", 66];
-  // 1. 참조값 복사
-  const e = d;
-  // 2. 각 원소를 복사해서 새 배열 생성
-  const { ...f } = d;
-  e[2] = 88;
-  console.log("e[2]> ", e[2]); // 88
-  console.log("f[2]> ", f[2]); // 66
-
-  return <div></div>;
+  return (
+    <div>
+      <button
+        style={{
+          padding: '10px',
+          color: 'white',
+          backgroundColor: 'blue',
+        }}
+      >
+        click1
+      </button>
+      <br />
+      <MyButton name="click2" />
+      <br />
+      <MyButton name="click3" />
+    </div>
+  );
 }
 
 export default App;
