ed2ed4e0e59cde9dd344fdf35d28da510c0a481a
huitopia <huitopia27@gmail.com>
2024-05-10 15:11:05 +0900

    props의 값 타입은 js 자료형, 값은 {} 안에 작성, string은 {} 생략 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      {/* props의 값 타입은 js 값 타입이며 String만 중괄호 생략 가능 */}
+      <MyComp
+        name="son"
+        age={3.14}
+        address={{ city: "seoul", country: "USA" }}
+        foods={["pizza", "coffee"]}
+        married={true}
+        action={function () {
+          alert("hi");
+        }}
+        someAction={() => alert("hello")}
+      />
+    </div>
+  );
 }
 
 export default App;
