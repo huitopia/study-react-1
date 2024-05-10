f3ce625c9f67a7c076ccfccdc32bc1d6e8e6f1a3
huitopia <huitopia27@gmail.com>
2024-05-10 12:40:16 +0900

    배열 복사를 분해할당의 나머지로 모두 해결 연습

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,17 @@ function App(props) {
   // 나머지 모두로 배열 복사 가능
   const [...b] = a; // 각 원소를 새 배열에 복사 (참조값 복사 X)
   console.log("b> ", b); // [6,7,8]
+
+  // 연습 : 객체 복사
+  const d = ["pizza", "son", 66];
+  // 1. 참조값 복사
+  const e = d;
+  // 2. 각 원소를 복사해서 새 배열 생성
+  const { ...f } = d;
+  e[2] = 88;
+  console.log("e[2]> ", e[2]); // 88
+  console.log("f[2]> ", f[2]); // 66
+
   return <div></div>;
 }
 
