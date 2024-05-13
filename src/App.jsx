d53f9c1079996c17a56fc5b5d4b08e0bb1510895
huitopia <huitopia27@gmail.com>
2024-05-13 11:02:51 +0900

    Chakra UI 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"blue"}>click</Button>
+        <Button colorScheme={"green"}>
+          notice<Badge>new</Badge>
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
