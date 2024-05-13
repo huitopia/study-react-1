15d7dcb1d1378c10adceff28890e120760d28281
huitopia <huitopia27@gmail.com>
2024-05-13 12:03:03 +0900

    re render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val, setVal] = useState(0);
+  console.log("re render");
+  return (
+    <div>
+      <button onClick={() => setVal(val + 1)}>click</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
