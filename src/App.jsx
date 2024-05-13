c64740d2a63277a4b3b40533f1cc86ddbf55f125
huitopia <huitopia27@gmail.com>
2024-05-13 15:35:34 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-import React, { useState } from "react";
-
-function MyComponent() {
-  return (
-    <ul>
-      <li>{list}</li>
-    </ul>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  function handleUpdateText(e) {
-    setText(e.target.value);
-  }
-
-  function handleUpdateList() {
-    const [...newList] = list;
-    newList.push(text);
-    setList(newList);
-  }
-
-  return (
-    <div>
-      <input type="text" onChange={handleUpdateText} value={text} />
-      <button onClick={handleUpdateList}>Add</button>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
