de8e96dc72f0d3dbf82376aac84c7ec3a06e906b
huitopia <huitopia27@gmail.com>
2024-05-13 15:18:00 +0900

    상태예제4) input에 입력한값 list에 추가해서 보여주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComponent() {
+  return (
+    <ul>
+      <li>{list}</li>
+    </ul>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  function handleUpdateText(e) {
+    setText(e.target.value);
+  }
+
+  function handleUpdateList() {
+    const [...newList] = list;
+    newList.push(text);
+    setList(newList);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleUpdateText} value={text} />
+      <button onClick={handleUpdateList}>Add</button>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
