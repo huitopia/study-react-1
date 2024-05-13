9b46eaeb3e1a5089ea8974fa415a37d168d4d0c0
huitopia <huitopia27@gmail.com>
2024-05-13 14:33:53 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-const foods = ["pizza", "pasta", "ramen", "taco", "coffee"];
 function App(props) {
-  const [index, setIndex] = useState(0);
-  const food = foods[index];
-
-  let count = 0;
-  function beforeClick() {
-    if (index <= 0) {
-      count = 0;
-    } else {
-      count = 1;
-    }
-    setIndex(index - count);
-  }
-  function nextClick() {
-    if (index == food.length) {
-      count = 0;
-    } else {
-      count = 1;
-    }
-    setIndex(index + count);
-  }
-  return (
-    <div>
-      <button onClick={beforeClick}>이전</button>
-      <button onClick={nextClick}>다음</button>
-      <div>{food}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
