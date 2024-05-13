d5efde8f3c3e81fd1b0d7caad6fe7e770e1f24f3
huitopia <huitopia27@gmail.com>
2024-05-13 09:48:30 +0900

    import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+// import : 다른 파일에 있는 변수를 현재 파일에서 사용
+import { value1, value2 } from "./MyValues.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <p>{value1}</p>
+      <p>{value2}</p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyValues.jsx
@@ -0,0 +1,5 @@
+// 다른 파일에서 사용 가능하도록 내보내기
+export const value1 = "h";
+export const value2 = "j";
+export const value3 = 22;
+export const value4 = { name: "name?", team: "team?" };
