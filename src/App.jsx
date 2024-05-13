5d2490a48d17ca4f784cd54edef388487c47b66b
huitopia <huitopia27@gmail.com>
2024-05-13 12:03:12 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val, setVal] = useState(0);
-  console.log("re render");
-  return (
-    <div>
-      <button onClick={() => setVal(val + 1)}>click</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
