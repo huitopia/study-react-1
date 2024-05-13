0784cc18819dd22fbfd1323b17d57c5e971494fd
huitopia <huitopia27@gmail.com>
2024-05-13 12:29:53 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
-function MyComp1() {
-  const [val, setVal] = useState(0);
-
-  function updateVal() {
-    // react는 re render 시점을 적절한 순간으로 선택
-    setVal(val + 1);
-    setVal(val + 1);
-    setVal(val + 1);
-  }
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={updateVal}>click</button>
-      </div>
-    </div>
-  );
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp1></MyComp1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
