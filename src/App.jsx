ce1b5c735af998f209fa77f3f8bdfc1bbd053f13
huitopia <huitopia27@gmail.com>
2024-05-13 10:27:05 +0900

    React Component의 import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-// import : 다른 파일에 있는 변수를 현재 파일에서 사용
-//  다른 파일의 같은 변수를 import 할 때 as로 다른 별칭 부여 가능
-import { value1, value2, value3 as v3 } from "./MyValues.jsx";
-import { myAddress, myStr } from "./MyComps.jsx";
+import { action1, action2, action3, MyComp } from "./MyComps.jsx";
+import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
+  action1();
+  action2();
+  action3();
   return (
     <div>
-      <p>{value1}</p>
-      <p>{value2}</p>
-      <p>{v3}</p>
-      <p>{myStr}</p>
-      <p>{myAddress}</p>
+      <MyComp />
+      <HerComp />
     </div>
   );
 }
--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,3 +1,16 @@
 export const myStr = "korea";
 export const myAddress = "seoul";
 export const value3 = 99;
+export function action1() {
+  return null;
+}
+export const action2 = function () {
+  return null;
+};
+export const action3 = () => {
+  return null;
+};
+
+export const MyComp = () => {
+  return <div>Lorem.</div>;
+};
