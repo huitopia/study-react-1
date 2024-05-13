b0891c3e14a0667608f9b80ffd5ee01948634133
huitopia <huitopia27@gmail.com>
2024-05-13 10:09:05 +0900

    as로 별칭 주기 가능, 여러 파일에 같은 변수가 있으면 as로 추올 피할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 // import : 다른 파일에 있는 변수를 현재 파일에서 사용
-import { value1, value2 } from "./MyValues.jsx";
+//  다른 파일의 같은 변수를 import 할 때 as로 다른 별칭 부여 가능
+import { value1, value2, value3 as v3 } from "./MyValues.jsx";
+import { myAddress, myStr } from "./MyComps.jsx";
 
 function App(props) {
   return (
     <div>
       <p>{value1}</p>
       <p>{value2}</p>
+      <p>{v3}</p>
+      <p>{myStr}</p>
+      <p>{myAddress}</p>
     </div>
   );
 }
