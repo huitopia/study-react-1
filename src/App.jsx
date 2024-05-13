cc8fbf26c021ed39b0149ad3d7ba6149e7115ec7
huitopia <huitopia27@gmail.com>
2024-05-13 11:03:19 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme={"blue"}>click</Button>
-        <Button colorScheme={"green"}>
-          notice<Badge>new</Badge>
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
