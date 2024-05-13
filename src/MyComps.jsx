788dd36f133e896da50d43e215fb407796cd6a35
huitopia <huitopia27@gmail.com>
2024-05-13 10:32:16 +0900

    react component 생성하는 방법

---
+++ src/MyComps.jsx
@@ -0,0 +1,11 @@
+export function MyComp() {
+  return <p>Lorem ipsum.</p>;
+}
+
+export const YourComp = function () {
+  return <div>youcomp</div>;
+};
+
+export const ArrowComp = () => {
+  return <div>Lorem ipsum dolor.</div>;
+};
