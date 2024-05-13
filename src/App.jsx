0708359bb71d473176be6f882beb111544505d77
huitopia <huitopia27@gmail.com>
2024-05-13 11:12:38 +0900

    default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+// default export는 아무 이름으로 중괄호 없이 import 가능
+import val, { v1 } from "./MyValues.jsx";
+import someVal from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>{v1}</div>
+      <div>{val}</div>
+      <div>{someVal}</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyValues.jsx
@@ -0,0 +1,6 @@
+// named export
+export const v1 = "a";
+
+// default export
+//  한 파일에서 하나만 가능
+export default "lee";
