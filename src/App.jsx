028e14a4cf4c1f194b60dc3edfe0d6a4955e43b3
huitopia <huitopia27@gmail.com>
2024-05-13 09:23:26 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+function MyComp({ name, age, children }) {
+  return (
+    <div>
+      <ul>
+        <li>{name}</li>
+        <li>{age}</li>
+      </ul>
+      {children}
+    </div>
+  );
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name={"son"} age={33}>
+        {/* content는 children porp*/}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+      <MyComp name={"lee"} age={22}>
+        <ul>
+          <li>Lorem.</li>
+          <li>Quibusdam!</li>
+        </ul>
+      </MyComp>
+    </div>
+  );
 }
 
 export default App;
