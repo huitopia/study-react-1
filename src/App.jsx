fec63662cc4fca257627d5de4a49e002bf15a0a4
huitopia <huitopia27@gmail.com>
2024-05-13 09:31:47 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, children }) {
-  return (
-    <div>
-      <ul>
-        <li>{name}</li>
-        <li>{age}</li>
-      </ul>
-      {children}
-    </div>
-  );
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={33}>
-        {/* content는 children porp*/}
-        <p>Lorem ipsum dolor.</p>
-      </MyComp>
-      <MyComp name={"lee"} age={22}>
-        <ul>
-          <li>Lorem.</li>
-          <li>Quibusdam!</li>
-        </ul>
-      </MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
