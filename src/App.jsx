f3a0ba8155c506f346b6788d84a3461830eac610
huitopia <huitopia27@gmail.com>
2024-05-13 15:05:19 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(event) {
-    setText(event.target.value);
-  }
-
-  return (
-    <div>
-      <input type="text" onChange={handleUpdateText} />
-      <p>{text}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
