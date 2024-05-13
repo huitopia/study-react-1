c31aaf47733fa5f2cca0cddbc39abd9ca50d5a08
huitopia <huitopia27@gmail.com>
2024-05-13 10:27:55 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { action1, action2, action3, MyComp } from "./MyComps.jsx";
-import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
-  action1();
-  action2();
-  action3();
-  return (
-    <div>
-      <MyComp />
-      <HerComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
