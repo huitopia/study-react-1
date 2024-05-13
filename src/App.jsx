d700497176b673c3886bcb423fee5c188eff950d
huitopia <huitopia27@gmail.com>
2024-05-13 12:28:17 +0900

    React가 컴포넌트를 다시 렌더링할 때 최적의 타이밍을 자동으로 결정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+function MyComp1() {
+  const [val, setVal] = useState(0);
+
+  function updateVal() {
+    // react는 re render 시점을 적절한 순간으로 선택
+    setVal(val + 1);
+    setVal(val + 1);
+    setVal(val + 1);
+  }
+
+  return (
+    <div>
+      <div>{val}</div>
+      <div>
+        <button onClick={updateVal}>click</button>
+      </div>
+    </div>
+  );
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp1></MyComp1>
+    </div>
+  );
 }
 
 export default App;
