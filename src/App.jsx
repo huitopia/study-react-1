684f6a338d8ba2a66be419f469ce1ae2cac8d2a7
huitopia <huitopia27@gmail.com>
2024-05-13 14:05:06 +0900

    상태예제1) 버튼 클릭하면 1씩 증가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyCounter() {
+  const [count, setCount] = useState(0);
+  function upCount() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={upCount}>증가</button>
+        {count}
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyCounter />
+    </div>
+  );
 }
 
 export default App;
