dfffc822af8d53f7f57a0827e82df1e291343331
huitopia <huitopia27@gmail.com>
2024-05-13 14:23:17 +0900

    상태예제2) state 변경되면 다시 그려짐

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
+const foods = ["pizza", "pasta", "ramen", "taco", "coffee"];
 function App(props) {
-  return <div></div>;
+  const [index, setIndex] = useState(0);
+  const food = foods[index];
+
+  let count = 0;
+  function beforeClick() {
+    if (index <= 0) {
+      count = 0;
+    } else {
+      count = 1;
+    }
+    setIndex(index - count);
+  }
+  function nextClick() {
+    if (index == food.length) {
+      count = 0;
+    } else {
+      count = 1;
+    }
+    setIndex(index + count);
+  }
+  return (
+    <div>
+      <button onClick={beforeClick}>이전</button>
+      <button onClick={nextClick}>다음</button>
+      <div>{food}</div>
+    </div>
+  );
 }
 
 export default App;
