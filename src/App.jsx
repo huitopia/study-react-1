01f3d05b74f12f74a4928fd3c0ec55b4f86f6dbb
huitopia <huitopia27@gmail.com>
2024-05-13 14:02:10 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp1() {
-  // state 여러 개 존재 가능
-  const [val1, setVal1] = useState(0);
-  const [val2, setVal2] = useState(0);
-  return (
-    <div>
-      <div>{val1}</div>
-      <div>
-        <button onClick={() => setVal1(val1 + 1)}>click1</button>
-      </div>
-      <div>{val2}</div>
-      <div>
-        <button onClick={() => setVal2(val2 - 1)}>click2</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp1 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
