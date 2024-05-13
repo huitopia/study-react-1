b6e108bc87be4db162a5e0a5a8373855e5527f13
huitopia <huitopia27@gmail.com>
2024-05-13 16:14:48 +0900

    state 예제 6) 하나의 state(object)가 여러 property를 갖는 경우

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [person, setPerson] = useState({
+    name: "",
+    address: "",
+    city: "",
+    email: "",
+  });
+
+  function handleUpdateName(e) {
+    const { ...nextPerson } = person;
+    nextPerson.name = e.target.value;
+    setPerson(nextPerson);
+  }
+
+  function handleUpdateEmail(e) {
+    const nextPerson = { ...person, city: e.target.value };
+    setPerson(nextPerson);
+  }
+  function handleUpdateCity(e) {
+    const { ...nextPerson } = person;
+    nextPerson.city = e.target.value;
+    setPerson(nextPerson);
+  }
+  function handleUpdateAddress(e) {
+    const { ...nextPerson } = person;
+    nextPerson.address = e.target.value;
+    setPerson(nextPerson);
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder={"name"} onChange={handleUpdateName} />
+      </div>
+      <div>
+        <input
+          type="email"
+          placeholder={"email"}
+          onChange={handleUpdateEmail}
+        />
+      </div>
+      <div>
+        <input type="text" placeholder={"city"} onChange={handleUpdateCity} />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder={"address"}
+          onChange={handleUpdateAddress}
+        />
+      </div>
+      <ul>
+        <li>name: {person.name}</li>
+        <li>email: {person.email}</li>
+        <li>city: {person.city}</li>
+        <li>address: {person.address}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
