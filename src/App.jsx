225aa7dccde0445624c0444f2e11646c13baa0f2
huitopia <huitopia27@gmail.com>
2024-05-13 09:13:44 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
-const App = () => {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button variant="outline" colorScheme={"red"}>
-          Click
-        </Button>
-        <Button colorScheme={"orange"}>Click</Button>
-        <Button colorScheme={"yellow"}>Click</Button>
-        <Button colorScheme={"green"}>Click</Button>
-        <Button colorScheme={"cyan"}>Click</Button>
-        <Button colorScheme={"blue"}>Click</Button>
-        <Button colorScheme={"purple"}>Click</Button>
-      </div>
-    </ChakraProvider>
-  );
-};
+function App(props) {
+  return <div></div>;
+}
 
 export default App;
