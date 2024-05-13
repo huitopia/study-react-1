7002a9b9989bba108233ff85781d683d2ac8a5a3
huitopia <huitopia27@gmail.com>
2024-05-13 16:03:14 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [age, setAge] = useState("");
-  const [name, setName] = useState("");
-  const [country, setCountry] = useState("");
-  const [team, setTeam] = useState("");
-  // 필요시 여러 state 사용 가능
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          placeholder={"나이"}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"이름"}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"국가"}
-          onChange={(e) => setCountry(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"팀"}
-          onChange={(e) => setTeam(e.target.value)}
-        />
-      </div>
-      <div>
-        {age}세 {name}은 {country}, {team} 소속입니다.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
