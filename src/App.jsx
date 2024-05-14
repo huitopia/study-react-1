f03f5c46b2c953b0b670dd1282db413ee847de6c
huitopia <huitopia27@gmail.com>
2024-05-14 11:25:57 +0900

    prop drilling code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ message }) {
+  return <div>{message}</div>;
+}
+
+function MySection({ message }) {
+  return (
+    <div>
+      <MyBox message={message} />
+    </div>
+  );
+}
+
+function MyContainer({ message }) {
+  return <MySection message={message} />;
+}
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState();
+  function handleMessage(event) {
+    setMessage(event.target.value);
+  }
+  return (
+    <div>
+      <input type="text" onChange={handleMessage} />
+      <p>{message}</p>
+      <hr />
+      <MyContainer message={message} />
+    </div>
+  );
 }
 
 export default App;
