2b3074dc7947f3f256db61d622bd8205ed8e7fa7
huitopia <huitopia27@gmail.com>
2024-05-14 16:34:16 +0900

    고객 번호로 고객 이름 조회

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [customerId, setCustomerId] = useState("");
+  const [customerName, setCustomerName] = useState("");
+
+  function handleSearchClick() {
+    // 조회버튼 클릭시
+    // /api/customer?id=3
+    axios
+      .get(`/api/customer?id=${customerId}`)
+      .then((response) => response.data)
+      .then((data) => setCustomerName(data));
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
+        <button onClick={handleSearchClick}>search</button>
+      </div>
+      <div>name: {customerName}</div>
+    </div>
+  );
 }
 
 export default App;
