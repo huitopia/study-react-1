df67f4540c3cc87e7b2ac0c479a73f8b4024c890
huitopia <huitopia27@gmail.com>
2024-05-14 09:47:30 +0900

    컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp({ color }) {
+  const [count, setCount] = useState(0);
+  function clickFunc() {
+    setCount(count + 1);
+  }
+  return (
+    <div style={{ border: `5px solid ${color}` }}>
+      <div>{count}</div>
+      <div>
+        <button onClick={clickFunc}>click</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [view1, setView1] = useState(true);
+  const [view2, setView2] = useState(true);
+  return (
+    <div>
+      <input
+        type={"checkbox"}
+        checked={view1}
+        onClick={(e) => setView1(e.target.checked)}
+      />
+      {view1 && <MyComp color={"blue"} />}
+      <hr />
+      <input
+        type={"checkbox"}
+        checked={view2}
+        onClick={(e) => setView2(e.target.checked)}
+      />
+      {view2 && <MyComp color={"red"} />}
+    </div>
+  );
 }
 
 export default App;
