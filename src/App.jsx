fdadf4a58e1ad4e18530e5f6ddc7b7cf7126dcba
huitopia <huitopia27@gmail.com>
2024-05-14 11:16:17 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-
-function MyButton({ count, onClick }) {
-  return (
-    <div>
-      <button onClick={onClick}>Up {count}</button>
-    </div>
-  );
-}
-
-function MyBox({ count }) {
-  return <div>{count}번 클릭됨</div>;
-}
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  function handleUpCount() {
-    setCount(count + 1);
-  }
-  return (
-    <div>
-      <MyButton count={count} onClick={handleUpCount} />
-      <hr />
-      <MyBox count={count} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
