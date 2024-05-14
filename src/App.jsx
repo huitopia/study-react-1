aa3c9988043ae02b880681c0c8c0e2dce9ee592c
huitopia <huitopia27@gmail.com>
2024-05-14 15:09:47 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount");
-  }, []);
-
-  useEffect(() => {
-    console.log("count Change");
-    // dependency 가 변경되지 않도록 주의
-    // setCount(count+1)
-  }, [count]);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>click {count}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
