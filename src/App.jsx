5a99dcc26ace39cba86deed15936633694f1f584
huitopia <huitopia27@gmail.com>
2024-05-14 14:11:01 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
-
-const TextContext = createContext("");
-function MyInput() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <input type="text" onChange={textHandler.handleText} />
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
-
-function MyText() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState();
-  function handleText(e) {
-    setText(e.target.value);
-  }
-  return (
-    <div>
-      <TextContext.Provider value={{ text, handleText }}>
-        <MyInput />
-        <MyText />
-      </TextContext.Provider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
