0227dc6854d61dd96b66cf9ce6bd5288ddc48de1
huitopia <huitopia27@gmail.com>
2024-05-14 12:24:41 +0900

    context로 data(object) 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+const TextContext = createContext("");
+function MyInput() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <input type="text" onChange={textHandler.handleText} />
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
+
+function MyText() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState();
+  function handleText(e) {
+    setText(e.target.value);
+  }
+  return (
+    <div>
+      <TextContext.Provider value={{ text, handleText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
+    </div>
+  );
 }
 
 export default App;
