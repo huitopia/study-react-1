1cfbdd0083697616c2f0aa502bb9b4abf58b7d22
huitopia <huitopia27@gmail.com>
2024-05-16 15:27:11 +0900

    응답 코드 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={() => axios.get("/api/main43/sub1")}>200</button>
+      <button onClick={() => axios.get("/api/main43/sub2")}>400</button>
+      <button onClick={() => axios.get("/api/main43/sub3")}>403</button>
+      <button onClick={() => axios.get("/api/main43/sub4")}>401</button>
+      <button onClick={() => axios.get("/api/main43/sub5")}>404</button>
+      <button onClick={() => axios.get("/api/main43/sub6")}>500</button>
+    </div>
+  );
 }
 
 export default App;
