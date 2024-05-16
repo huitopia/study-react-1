90b3816b66cfa6b4bf56c11158dfa9ca46b99315
huitopia <huitopia27@gmail.com>
2024-05-16 15:21:31 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [result, setResult] = useState();
-  function handleClick1() {
-    axios.get("/api/main42/sub1").then((res) => {
-      console.log(res.data);
-    });
-  }
-
-  function handleClick2() {
-    axios.get("/api/main42/sub2").then((res) => {
-      setResult(res.data);
-    });
-  }
-
-  function handleClick3() {
-    axios.get("/api/main42/sub3").then((res) => {
-      console.log(res.data.info);
-    });
-  }
-
-  function handleClick4() {
-    axios.get("/api/main42/sub4").then((res) => {
-      console.log(res.data);
-    });
-  }
-
-  function handleClick5() {
-    axios.get("/api/main42/sub5").then((res) => {
-      console.log(res.data);
-    });
-  }
-
-  function handleClick6() {
-    axios.get("/api/main42/sub6").then((res) => {
-      console.log(res.data);
-    });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>응답 받기</button>
-      <br />
-      <button onClick={handleClick2}>res : {result}</button>
-      <br />
-      <button onClick={handleClick3}>ResponseBody</button>
-      <br />
-      <button onClick={handleClick4}>Json</button>
-      <br />
-      <button onClick={handleClick5}>응답 받기</button>
-      <br />
-      <button onClick={handleClick6}>응답 받기</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
