04f61d116ceceb9af72e523a3a28d5a29867bce1
huitopia <huitopia27@gmail.com>
2024-05-16 12:06:06 +0900

    input에 입력한 값 json으로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState();
+  const [city, setCity] = useState();
+  const [iphone, setIphone] = useState("");
+  const [version, setVersion] = useState(0);
+
   function handleClick1() {
     const obj1 = { name: "chae", age: 7 };
     // const json1 = JSON.stringify(obj1);
@@ -18,9 +23,6 @@ function App(props) {
     axios.post("/api/main41/sub2", obj2);
   }
 
-  const [name, setName] = useState();
-  const [city, setCity] = useState();
-
   function handleClick3() {
     const data = {
       // 변수명과 property명이 같을 때 생략 가능
@@ -28,6 +30,14 @@ function App(props) {
       city,
     };
     axios.post("/api/main41/sub3", data);
+  }
+
+  function handleClick4() {
+    const data = {
+      iphone: iphone,
+      version: version,
+    };
+    axios.post("/api/main41/sub4", data);
   }
 
   return (
@@ -40,6 +50,13 @@ function App(props) {
         <input type="text" onChange={(e) => setCity(e.target.value)} />
         <br />
         <button onClick={handleClick3}>input post</button>
+      </div>
+      <div>
+        <input type="text" onChange={(e) => setIphone(e.target.value)} />
+        <br />
+        <input type="text" onChange={(e) => setVersion(e.target.value)} />
+        <br />
+        <button onClick={handleClick4}>create</button>
       </div>
     </div>
   );
