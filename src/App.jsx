f1ead118edc68adac12507b38efcef14c83e6991
huitopia <huitopia27@gmail.com>
2024-05-16 14:02:56 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  // json data type
-  function handleClick1() {
-    const data = {
-      name: "chae",
-      age: 7,
-      married: false,
-      apple: {
-        iphone: "pro",
-        macBook: "pro",
-      },
-      food: ["마라탕", "엽떡", "술"],
-      house: null,
-    };
-    axios.post("/api/main41/sub5", data);
-  }
-
-  function handleClick2() {
-    axios.post("/api/main41/sub6", { name: "chae", age: 7 });
-  }
-
-  function handleClick3() {
-    axios.post("/api/main41/sub7", { name: "chch", age: 27 });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>json data request</button>
-      <br />
-      <button onClick={handleClick2}>json data request</button>
-      <br />
-      <button onClick={handleClick3}>json data request</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
