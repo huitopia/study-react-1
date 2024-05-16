160b07d1196615b935f2501cb20b1975546378de
huitopia <huitopia27@gmail.com>
2024-05-16 14:19:41 +0900

    response.data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((res) => {
+      console.log(res.data);
+    });
+  }
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((res) => {
+      console.log(res.data);
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>응답 받기</button>
+      <br />
+      <button onClick={handleClick2}>res</button>
+    </div>
+  );
 }
 
 export default App;
