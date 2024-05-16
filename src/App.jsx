7639c2b782c6702955b4b2001754959ff8066ba0
huitopia <huitopia27@gmail.com>
2024-05-16 09:36:21 +0900

    /api/someurl의 get, post ajax 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    // get request
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    axios.post("/api/someurl");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get</button>
+      <button onClick={handleClickButton2}>post</button>
+    </div>
+  );
 }
 
 export default App;
