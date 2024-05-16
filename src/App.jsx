b8097282cd81e75cd82caac1768026b8be70bdfc
huitopia <huitopia27@gmail.com>
2024-05-16 11:23:52 +0900

    객체를 json으로 직렬화해서 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    const obj1 = { name: "chae", age: 7 };
+    // const json1 = JSON.stringify(obj1);
+    // axios에서 직렬화 해줌
+    axios.post("/api/main41/sub1", obj1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json request</button>
+    </div>
+  );
 }
 
 export default App;
