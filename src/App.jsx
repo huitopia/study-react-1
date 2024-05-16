109f51c52f11446754dd1736bb3e63efcbbc3451
huitopia <huitopia27@gmail.com>
2024-05-16 10:04:02 +0900

    /api/someurl2의 get, post ajax 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,24 @@ function App(props) {
     axios.post("/api/someurl");
   }
 
+  function handleClickBtn1() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickBtn2() {
+    axios.post("api/someurl2");
+  }
+
   return (
     <div>
-      <button onClick={handleClickButton1}>get</button>
-      <button onClick={handleClickButton2}>post</button>
+      <div>
+        <button onClick={handleClickButton1}>get</button>
+        <button onClick={handleClickButton2}>post</button>
+      </div>
+      <div>
+        <button onClick={handleClickBtn1}>get2</button>
+        <button onClick={handleClickBtn2}>post2</button>
+      </div>
     </div>
   );
 }
