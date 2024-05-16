e7bbf281524da79fb2fa6626fc22d07c85236f83
huitopia <huitopia27@gmail.com>
2024-05-16 12:18:39 +0900

    js 객체가 json 으로 직렬화될때 각 data type 사용됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  // json data type
+  function handleClick1() {
+    const data = {
+      name: "chae",
+      age: 7,
+      married: false,
+      apple: {
+        iphone: "pro",
+        macBook: "pro",
+      },
+      food: ["마라탕", "엽떡", "술"],
+      house: null,
+    };
+    axios.post("/api/main41/sub5", data);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json data request</button>
+    </div>
+  );
 }
 
 export default App;
