deec19271bc2148a9ffbd9c33299969db5abbff6
huitopia <huitopia27@gmail.com>
2024-05-16 11:00:54 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  function handleClickButton1() {
-    // get request
-    axios.get("/api/someurl");
-  }
-
-  function handleClickButton2() {
-    axios.post("/api/someurl");
-  }
-
-  function handleClickButton3() {
-    axios.get("/api/someurl2");
-  }
-
-  function handleClickButton4() {
-    axios.post("api/someurl2");
-  }
-
-  function handleClickButton5() {
-    const params = new URLSearchParams();
-    params.append("id", 3);
-    params.append("name", "k");
-    params.append("email", "k@k.com");
-    axios.get(`/api/someurl3?${params}`);
-  }
-
-  function handleClickButton6() {
-    const params = new URLSearchParams({
-      name: "son",
-      age: 2,
-    });
-    axios.post(`/api/someurl3?${params}`);
-  }
-
-  function handleClickButton7() {
-    const params = new URLSearchParams({
-      age: 10,
-      job: "student",
-    });
-    axios.get(`/api/someurl4?${params}`);
-  }
-
-  function handleClickButton8() {
-    const params = new URLSearchParams({
-      id: 1,
-      name: "kim",
-    });
-    axios.post(`/api/someurl4?${params}`);
-  }
-
-  return (
-    <div>
-      <div>
-        <button onClick={handleClickButton1}>get</button>
-        <button onClick={handleClickButton2}>post</button>
-      </div>
-      <div>
-        <button onClick={handleClickButton3}>get2</button>
-        <button onClick={handleClickButton4}>post2</button>
-      </div>
-      <div>
-        <button onClick={handleClickButton5}>get query string</button>
-        <button onClick={handleClickButton6}>post data</button>
-      </div>
-      <div>
-        <button onClick={handleClickButton7}>get query string</button>
-        <button onClick={handleClickButton8}>post data</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
