0539dcc0e64b2cff7c38a62c5c6e641daa1d6e69
huitopia <huitopia27@gmail.com>
2024-05-16 09:21:51 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [customerId, setCustomerId] = useState("");
-  const [customerName, setCustomerName] = useState("");
-
-  function handleSearchClick() {
-    // 조회버튼 클릭시
-    // /api/customer?id=3
-    axios
-      .get(`/api/customer?id=${customerId}`)
-      .then((response) => response.data)
-      .then((data) => setCustomerName(data));
-  }
-
-  return (
-    <div>
-      <div>
-        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
-        <button onClick={handleSearchClick}>search</button>
-      </div>
-      <div>name: {customerName}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
