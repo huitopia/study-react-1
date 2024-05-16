eb8a3b65521bc5387885cfd78b2f1c6539d36994
huitopia <huitopia27@gmail.com>
2024-05-16 15:08:46 +0900

    json 응답 파싱 하기

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,12 @@ function App(props) {
     });
   }
 
+  function handleClick6() {
+    axios.get("/api/main42/sub6").then((res) => {
+      console.log(res.data);
+    });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
@@ -44,6 +50,8 @@ function App(props) {
       <button onClick={handleClick4}>Json</button>
       <br />
       <button onClick={handleClick5}>응답 받기</button>
+      <br />
+      <button onClick={handleClick6}>응답 받기</button>
     </div>
   );
 }
