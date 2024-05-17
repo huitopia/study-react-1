e22ca987a5c5bd8d8119188ddcbb0ed9df804c38
huitopia <huitopia27@gmail.com>
2024-05-17 16:01:19 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
 
-const SpringRoot = () => {
-  return (
-    <div>
-      <div
-        style={{
-          padding: "10px",
-          backgroundColor: "#eee",
-          display: "flex",
-          gap: "10px",
-        }}
-      >
-        <div>
-          <Link to="/spring/learn">Learn</Link>
-        </div>
-        <div>
-          <Link to="/spring/api">Api</Link>
-        </div>
-        <div>
-          <Link to="/spring/doc">Doc</Link>
-        </div>
-      </div>
-      <div style={{ marginTop: "20px" }}>
-        <Outlet />
-      </div>
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "spring",
-    element: <SpringRoot />,
-    children: [
-      {
-        index: true,
-        element: <div>spring main page</div>,
-      },
-      {
-        path: "api",
-        element: <div>api page</div>,
-      },
-      {
-        path: "doc",
-        element: <div>doc page</div>,
-      },
-      {
-        path: "learn",
-        element: <div>learn page</div>,
-      },
-    ],
-  },
-]);
 function App(props) {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
