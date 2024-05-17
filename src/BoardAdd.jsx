b29e879ef52d2d862b1b502cd6b897f317fe6872
huitopia <huitopia27@gmail.com>
2024-05-17 15:02:34 +0900

    code 정리

--- src/BoardAdd.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const BoardAdd = () => {
-  return <div>add page</div>;
-};
--- src/BoardEdit.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const BoardEdit = () => {
-  <div>edit page</div>;
-};
