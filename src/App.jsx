ebe852621541c897ce17781928eb1253fb730276
huitopia <huitopia27@gmail.com>
2024-05-17 15:02:07 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { BoardAdd } from "./BoardAdd.jsx";
-import { BoardEdit } from "./BoardEdit.jsx";
-import { BoardList } from "./BoardList.jsx";
 
-// router
-const router = createBrowserRouter([
-  {
-    path: "/add",
-    element: <BoardAdd />,
-  },
-  {
-    path: "/edit",
-    element: <BoardEdit />,
-  },
-  {
-    path: "list",
-    element: <BoardList />,
-  },
-  {
-    path: "/board",
-    children: [
-      {
-        path: "write",
-        element: <div>board write</div>,
-      },
-      {
-        path: "edit",
-        element: <div>board edit</div>,
-      },
-    ],
-  },
-]);
 function App(props) {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
