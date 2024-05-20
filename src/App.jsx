50688a4cd425a7bc69b89c266b78e17452146ffc
huitopia <huitopia27@gmail.com>
2024-05-20 10:25:54 +0900

    sx : 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+
+function MyComp() {
+  return (
+    <div>
+      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
+        click
+      </Button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
