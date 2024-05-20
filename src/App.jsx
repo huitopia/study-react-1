3b965d2a68b21ee46c0d8efc278229fb5dcee080
huitopia <huitopia27@gmail.com>
2024-05-20 10:18:41 +0900

    Responsive Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function MyComp() {
+  return (
+    <Box
+      fontSize={{ base: "11px", md: "15px", lg: "20px" }}
+      bgColor={"blue.200"}
+      w={"1200px"}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem earum
+      maiores, odit quaerat quis quisquam.
+    </Box>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
