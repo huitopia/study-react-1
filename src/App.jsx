a22daa37bc6e7de5539ddd68f8cf396687017d2f
huitopia <huitopia27@gmail.com>
2024-05-20 09:59:00 +0900

    Chakra UI box, color, margin, width, height, padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function MyBox() {
+  return (
+    <Box
+      textTransform="uppercase"
+      fontSize="xs"
+      fontWeight="semibold"
+      lineHeight="10"
+      bg="blue.300"
+      w="50%"
+      h={"80px"}
+      p={4}
+      color="white"
+      borderWidth="1px"
+      mb="2"
+    >
+      This is the Box
+    </Box>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box>
+        <MyBox />
+        <MyBox />
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
