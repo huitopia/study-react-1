2d6772e6a8aa6f80451cdbf4da63999d06328858
huitopia <huitopia27@gmail.com>
2024-05-20 14:28:51 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
-import { Box, Button, ChakraProvider } from "@chakra-ui/react";
-
-function MyComp() {
-  function handleButtonClick(e) {
-    e.stopPropagation();
-    console.log("button clicked!");
-  }
-
-  function handleBoxClick() {
-    console.log("box clicked!");
-  }
-
-  return (
-    <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
-      <Button onClick={handleButtonClick}>click</Button>
-    </Box>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <MyComp />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
