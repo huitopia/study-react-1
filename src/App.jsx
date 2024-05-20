f22fe6842360c2aedfc5a974842d58ea468f2b5a
huitopia <huitopia27@gmail.com>
2024-05-20 11:19:38 +0900

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function MyComp() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <div>
+      <Button onClick={onOpen}>open</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>title</ModalHeader>
+            <ModalBody>Lorem ipsum dolor.</ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>close</Button>
+              <Button colorScheme={"blue"}>save</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
