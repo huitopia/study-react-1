50c042340327396fdefd05441e0547bfa368c39e
huitopia <huitopia27@gmail.com>
2024-05-20 10:45:14 +0900

    FormControl

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+function MyComp() {
+  return (
+    <div>
+      <FormControl>
+        <FormLabel>name</FormLabel>
+        <Input />
+      </FormControl>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
