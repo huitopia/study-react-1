469a5aa7570c6b5d9143e00f92b465475e0fd788
huitopia <huitopia27@gmail.com>
2024-05-20 10:18:49 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
-
-function MyComp() {
-  return (
-    <Box
-      fontSize={{ base: "11px", md: "15px", lg: "20px" }}
-      bgColor={"blue.200"}
-      w={"1200px"}
-    >
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem earum
-      maiores, odit quaerat quis quisquam.
-    </Box>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <MyComp />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
