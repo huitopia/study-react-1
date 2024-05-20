8ccefbfccf4585efdac01381bb186045210815ef
huitopia <huitopia27@gmail.com>
2024-05-20 10:22:34 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
-
-function MyComp() {
-  return (
-    <div>
-      <Box
-        bgColor={"blue.200"}
-        w={"100px"}
-        _hover={{
-          bgColor: "pink.100",
-        }}
-      >
-        Lorem ipsum dolor sit.
-      </Box>
-    </div>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <MyComp />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
