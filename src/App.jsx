7d756684bcd2cdb9c13f919a6920b7bdd1685482
huitopia <huitopia27@gmail.com>
2024-05-20 11:03:25 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import {
-  ChakraProvider,
-  FormControl,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
-
-function MyComp() {
-  return (
-    <div>
-      <FormControl>
-        <FormLabel>name</FormLabel>
-        <Input />
-      </FormControl>
-    </div>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <MyComp />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
