50d9d247c7000d6ef5b55cfc1b5210fcf5a1dbdd
huitopia <huitopia27@gmail.com>
2024-05-20 14:19:56 +0900

    이벤트 버블링 막기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
+
+function MyComp() {
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("button clicked!");
+  }
+
+  function handleBoxClick() {
+    console.log("box clicked!");
+  }
+
+  return (
+    <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
+      <Button onClick={handleButtonClick}>click</Button>
+    </Box>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
