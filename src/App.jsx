b5444f1eeda25dc053502b2e19a00ce7c13de084
huitopia <huitopia27@gmail.com>
2024-05-20 14:35:02 +0900

    기본 동작 막기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleLinkClick(e) {
+    e.preventDefault(); // 기본 이벤트 동작 막기
+    //
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("동작 코드");
+  }
+
+  return (
+    <div>
+      <a href="https://www.naver.com" onClick={handleLinkClick}>
+        {" "}
+        네이버{" "}
+      </a>
+      <hr />
+      <form action="https://www.naver.com" onSubmit={handleSubmit}>
+        <input type="text" />
+        <button>go</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
