049b0c844bc593af79e3bd3f6c5eadd9e22cef47
huitopia <huitopia27@gmail.com>
2024-05-20 10:22:27 +0900

    hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function MyComp() {
+  return (
+    <div>
+      <Box
+        bgColor={"blue.200"}
+        w={"100px"}
+        _hover={{
+          bgColor: "pink.100",
+        }}
+      >
+        Lorem ipsum dolor sit.
+      </Box>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyComp />
+    </ChakraProvider>
+  );
 }
 
 export default App;
