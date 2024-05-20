7527c562f52af675f74c6b62f2690675fc7dbc05
huitopia <huitopia27@gmail.com>
2024-05-20 15:04:00 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    e.preventDefault(); // 기본 이벤트 동작 막기
-    //
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("동작 코드");
-  }
-
-  return (
-    <div>
-      <a href="https://www.naver.com" onClick={handleLinkClick}>
-        {" "}
-        네이버{" "}
-      </a>
-      <hr />
-      <form action="https://www.naver.com" onSubmit={handleSubmit}>
-        <input type="text" />
-        <button>go</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
