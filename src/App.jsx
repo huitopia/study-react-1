b48be32e7c878f483aec41c48b376c47bb7aee1e
huitopia <huitopia27@gmail.com>
2024-05-20 11:38:16 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  const toast = useToast();
-  return (
-    <ChakraProvider>
-      <Button
-        onClick={() => {
-          toast({
-            title: "title",
-            description: "desc",
-            position: "top",
-            status: "success",
-            isClosable: true,
-            duration: 1500,
-          });
-        }}
-      ></Button>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
