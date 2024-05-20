66c4b7c49e447693cedec7736079e94914b0fe92
huitopia <huitopia27@gmail.com>
2024-05-20 09:26:01 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
 import React from "react";
-import { Button, ButtonGroup, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <div>size="sm"</div>
-        <ButtonGroup size="sm">
-          <Button colorScheme={"yellow"}>Button</Button>
-          <Button colorScheme={"red"}>Lorem.</Button>
-          <Button colorScheme={"orange"}>Facilis.</Button>
-          <Button colorScheme={"teal"}>Eum.</Button>
-          <Button colorScheme={"blue"}>Officia.</Button>
-          <Button colorScheme={"cyan"}>Eius.</Button>
-          <Button colorScheme={"gray"}>Doloribus.</Button>
-        </ButtonGroup>
-        <hr />
-        <Button colorScheme={"blue"} isLoading={true}>
-          Lorem
-        </Button>
-        <hr />
-        <Button variant="outline" colorScheme="blue">
-          Save
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
