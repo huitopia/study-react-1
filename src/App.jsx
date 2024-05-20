31cdb129cc77924a8b71117c2039eadcc0909db0
huitopia <huitopia27@gmail.com>
2024-05-20 10:06:35 +0900

    spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function MyBox1() {
+  return (
+    <Box bgColor={"tomato"} m={1}>
+      Lorem ipsum dolor sit.
+    </Box>
+  );
+}
+function MyBox2() {
+  return (
+    <Box bgColor={"blue.300"} m={2}>
+      Lorem ipsum dolor sit.
+    </Box>
+  );
+}
+function MyBox3() {
+  return (
+    <Box m={10} bgColor={"pink"}>
+      Lorem ipsum dolor sit.
+    </Box>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <MyBox1 />
+      <MyBox2 />
+      <MyBox3 />
+    </ChakraProvider>
+  );
 }
 
 export default App;
