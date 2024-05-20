d32e03c9857088a54efa340748d58a68667723cb
huitopia <huitopia27@gmail.com>
2024-05-20 10:02:56 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
-
-function MyBox() {
-  return (
-    <Box
-      textTransform="uppercase"
-      fontSize="xs"
-      fontWeight="semibold"
-      lineHeight="10"
-      bg="blue.300"
-      w="50%"
-      h={"80px"}
-      p={4}
-      color="white"
-      borderWidth="1px"
-      mb="2"
-    >
-      This is the Box
-    </Box>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Box>
-        <MyBox />
-        <MyBox />
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
