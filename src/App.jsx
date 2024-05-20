384d601700b2cdf907fb7303e04027c9a3e7cfef
huitopia <huitopia27@gmail.com>
2024-05-20 12:35:28 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  // use... : hook
-  // if / for 내에서 사용 X
-  return (
-    <ChakraProvider>
-      <div></div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
