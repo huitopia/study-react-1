200811e19294c7f8d145f61b24b744b265feaf5f
huitopia <huitopia27@gmail.com>
2024-05-20 11:29:25 +0900

    code 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,7 @@
 import React from "react";
-import {
-  Button,
-  ChakraProvider,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
-
-function MyComp() {
-  const { isOpen, onClose, onOpen } = useDisclosure();
-  return (
-    <div>
-      <Button onClick={onOpen}>open</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader>title</ModalHeader>
-            <ModalBody>Lorem ipsum dolor.</ModalBody>
-            <ModalFooter>
-              <Button onClick={onClose}>close</Button>
-              <Button colorScheme={"blue"}>save</Button>
-            </ModalFooter>
-          </ModalContent>
-        </ModalOverlay>
-      </Modal>
-    </div>
-  );
-}
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <MyComp />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
