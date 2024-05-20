689aed51108e46bdc15e0a663331253972b422e5
huitopia <huitopia27@gmail.com>
2024-05-20 11:38:04 +0900

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  const toast = useToast();
+  return (
+    <ChakraProvider>
+      <Button
+        onClick={() => {
+          toast({
+            title: "title",
+            description: "desc",
+            position: "top",
+            status: "success",
+            isClosable: true,
+            duration: 1500,
+          });
+        }}
+      ></Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
